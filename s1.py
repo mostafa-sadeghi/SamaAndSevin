97dadb04db651811c8efa519a8d89fe9e8416bef
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-06 16:57:23 +0330

    sadeghi

---
+++ s1.py
@@ -0,0 +1,75 @@
+# for i in range(5):
+#     print("hello")
+# print("outside")
+
+
+# for i in range(10):
+#     print("python")
+#     print("pygame")
+
+# for x in range(10):
+#     print(x)
+
+# for i in range(1, 11):
+#     print(i)
+
+# for i in range(2, 12, 2):
+#     print(i)
+
+# for i in range(1, 11, 2):
+#     print(i)
+
+
+# for number in range(10,0,-1):
+#     print(number)
+
+# for i in [1,4,8,-2,0]:
+#     print(i)
+
+# for s in 'python':
+#     print(s, end=' ')
+
+# یک برنامه بنویسید که نام شخصی را از ورودی دریافت نماید و 
+# تک تک کاراکترهای آن را پرینت کند
+
+
+# for i in range(4):
+#     print("hi")
+
+# for j in range(4):
+#     print("hello")
+
+# for i in range(4):
+#     print("hi")
+#     for j in range(4):
+#         print("hello")
+
+# numbers = [1,6,3,5,9]
+# for i in range(5):
+#     print(numbers[i])
+
+# برنامه ای بنویسید که پنج عدد از ورودی دریافت نماید و
+# در لیستی ذخیره کند
+
+# numbers = []
+# for i in range(5):
+#     new_number = int(input('enter a number: '))
+    # 
+
+# در نهایت لیست را پرینت نمائید
+
+
+total = 0
+for i in range(3):
+    new_number = int(input('enter a number: '))
+    total = total + new_number
+
+print(total)
+
+
+total = 0
+for x in range(100,301):
+    print(x)
+    total = total + x
+
+print(total)
