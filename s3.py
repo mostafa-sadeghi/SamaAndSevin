ce75b6d22d9ab9434971e0333a160ff595241289
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-20 16:59:58 +0330

    sadeghi

---
+++ s3.py
@@ -0,0 +1,77 @@
+#  1 + 2 + 3 + 4 + 5 + .... + 100
+# total = 0
+# for i in range(1,101):
+#     total = total + i
+#     # total += i
+# print("total is:",total)
+
+# 2 + 4 + 6 + 8 + ... + 100
+# total = 0
+# for n in range(2,101,2):
+#     total += n
+# print("total is: ", total)
+
+# 1 + 3 + 5 + 7 + ... + 99
+
+
+# for i in range(3):
+#     print("a")
+
+# for j in range(3):
+#     print("b")
+
+
+# for i in range(3):
+#     print("a")
+
+#     for j in range(3):
+#         print("b")
+
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(10,0,-1):
+#     print(i)
+
+
+# for i in range(3):
+#     print("hello")
+# i = 0
+# while i < 3:
+#     print("hello")
+#     i = i + 1
+
+
+# quit = "no"
+# while quit == "no":
+#     quit = input('Do you want to quit?(yes or no) ')
+
+
+# done = False
+# while not done:
+#     quit = input('Do you want to quit? ')
+#     if quit == "yes":
+#         done = True
+
+#     attack = input("Does the elf attacked the dragon? ")
+#     if attack == "yes":
+#         print("Bad choice, you died")
+#         done = True
+
+
+#  با حلقه while
+
+
+#  1 + 2 + 3 + 4 + 5 + .... + 100
+total = 0
+i = 0
+while i < 100:
+    total = total + i
+    i = i + 1
+print(total)
+
+# 2 + 4 + 6 + 8 + ... + 100
+
+
+# 1 + 3 + 5 + 7 + ... + 99
