9bdf0d69e9ce077420bad9b64e5e649d38b3cd83
msa <msai.sadeghi@gmail.com>
2023-10-21 16:58:08 +0330

    s

--- snake_game.py
+++ snake_game.py
@@ -1,11 +1,17 @@
 import turtle
-from snake_utils import create_object
+from snake_utils import *
 
 main_screen = turtle.Screen()
 main_screen.bgcolor("black")
 main_screen.setup(600, 600)
 main_screen.title("~~~Snake~~Game~~~")
+main_screen.tracer(False)
 root = main_screen._root
+
+snake_head = create_object("square", "green")
+snake_food = create_object("circle", "red")
+change_position(snake_food)
+# TODO Add poison apple
 
 
 def close():
--- snake_utils.py
+++ snake_utils.py
@@ -1,10 +1,18 @@
 import turtle
+import random
 
 
 def create_object(shape, color):
     my_turtle = turtle.Turtle()
     my_turtle.shape(shape)
     my_turtle.color(color)
+
     my_turtle.penup()
     my_turtle.speed("fastest")
     return my_turtle
+
+
+def change_position(object):
+    x = random.randint(-270, 270)
+    y = random.randint(-270, 230)
+    object.goto(x, y)
