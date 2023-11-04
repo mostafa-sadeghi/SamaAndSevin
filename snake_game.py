7ea8e47df732e2b3df8605888958e5175f3acc14
msa <msai.sadeghi@gmail.com>
2023-11-04 17:00:36 +0330

    S

--- snake_game.py
+++ snake_game.py
@@ -39,21 +39,59 @@ def move():
 
 
 def change_dir_to_up():
-    """
-    This function is used to change snake direction to up
-    """
     if snake_head.direction != "down":
         snake_head.direction = "up"
-# TODO  تعریف تابع برای سایر جهت ها
+
+
+def change_dir_to_down():
+    if snake_head.direction != "up":
+        snake_head.direction = "down"
+
+
+def change_dir_to_left():
+    if snake_head.direction != "right":
+        snake_head.direction = "left"
+
+
+def change_dir_to_right():
+    if snake_head.direction != "left":
+        snake_head.direction = "right"
 
 
 main_screen.listen()
 main_screen.onkeypress(change_dir_to_up, "Up")
-# صدازدن تابع برای سایر جهت ها
+main_screen.onkeypress(change_dir_to_down, "Down")
+main_screen.onkeypress(change_dir_to_left, "Left")
+main_screen.onkeypress(change_dir_to_right, "Right")
+
 
 root.protocol("WM_DELETE_WINDOW", close)
+
+score = 0
+
+scoreboard = create_object("square", "purple")
+scoreboard.goto(0, 260)
+scoreboard.hideturtle()
+scoreboard.write(f'Score:{score}', align="center", font=("Arial", 22))
+
+snake_tails = []
+
 running = True
 while running:
     main_screen.update()
+    if snake_head.distance(snake_food) < 20:
+        change_position(snake_food)
+        score += 1
+        new_tail = create_object("square", "darkgreen")
+        snake_tails.append(new_tail)
+
+    for i in range(len(snake_tails) - 1, 0, -1):
+        x = snake_tails[i-1].xcor()
+        y = snake_tails[i-1].ycor()
+        snake_tails[i].goto(x, y)
+
+    scoreboard.clear()
+    scoreboard.write(f'Score:{score}', align="center", font=("Arial", 22))
+
     move()
     sleep(0.2)
